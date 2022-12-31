6a13442026a52f412daf1a105e9fb4c97c09a963
hieul98 <hieule1998@yahoo.com>
2022-12-30 19:03:09 -0700

    continued work on capstone project

--- final_project_game/main_game_app/admin.py
+++ final_project_game/main_game_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import item, player
+from .models import attack_item, defense_item, player
 
 # Register your models here.
-admin.site.register(item)
+admin.site.register(attack_item)
 admin.site.register(player)
+admin.site.register(defense_item)
--- final_project_game/main_game_app/models.py
+++ final_project_game/main_game_app/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class player(models.Model):
-    name = models.CharField(max_length=30)
-
+class attack_item(models.Model):
+    name = models.CharField(max_length=500)
+    attack = models.IntegerField()
     def __str__(self):
         return self.name
 
-class item(models.Model):
+class defense_item(models.Model):
     name = models.CharField(max_length=500)
+    defense = models.IntegerField()
+    def __str__(self):
+        return self.name
 
+class player(models.Model):
+    name = models.CharField(max_length=30)
+    attack_items = models.ManyToManyField(attack_item, related_name='players')
+    defense_items = models.ManyToManyField(defense_item, related_name='players')
     def __str__(self):
         return self.name
